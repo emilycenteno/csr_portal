f0a5b0432e6a82774cac902477440bf28f181410
Emily Centeno <emilycenteno@Mac.lan>
2025-06-22 23:34:34 -0400

    mock data, displaying with CustomerCard

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,21 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import CustomerCard from './components/CustomerCard';
+import customerData from './tests/MOCK_DATA.json';
 
 
 
+
+console.log(customerData);
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <CustomerCard name = "Emily" />
-      <CustomerCard name = "Erik" />
-      <p>Mikey is stinky</p>
+
+    {customerData.map((item, index) => (<CustomerCard key={index} customer = {item} />))}
+      
+
     </>
   )
 }
--- src/components/CustomerCard.jsx
+++ src/components/CustomerCard.jsx
@@ -1,5 +1,53 @@
-export default function CustomerCard({name}) {
+import customerData from '../tests/MOCK_DATA.json';
+
+
+
+export default function CustomerCard({ customer }) {
+    console.log(customerData);
+
     return (
-        <h1>Hello there, {name}</h1>
+        <div className="customer">
+            <h2>{customer.last_name + ', ' + customer.first_name}</h2>
+            <h3>Contact Info:</h3>
+            <p><b>Email:</b> {customer.email}</p>
+            <p><b>Phone number:</b> {customer.phone_number}</p>
+            <h3>Address:</h3>
+            <Address address={customer.address} />
+            <h3>Cars</h3>
+            {customer.cars.map((car, index) => (<CarInfo key = {index} car = {car} />))}
+            <h3>Payment Info</h3>
+            <PaymentInfo creditCard={customer.credit_card} />
+            
+        </div>
+    )
+}
+
+function Address({ address }) {
+    return (
+            <p>
+                {address.street} < br />
+                {address.line_2 ? address.line_2 : null } 
+                {address.line_2 && < br />}
+                {address.city + ' ' + address.state + ', ' + address.postal_code}
+            </p>
+    )
+}
+function CarInfo({car}) {
+    return (
+        <p>
+            <b>Car Make:</b> {car.make} <br />
+            <b>Car Model:</b> {car.model} <br />
+            <b>Car Year:</b> {car.year} <br />
+            <b>Car Vin: </b> {car.vin} <br />
+        </p>
+    )
+}
+function PaymentInfo({creditCard}) {
+    return (
+        <p>
+            <b>Card Type:</b> {creditCard.type} <br />
+            <b>Card Number: </b> {creditCard.number} <br />
+            <b>Billing Zip: </b> {creditCard.zip}
+        </p>
     )
 }
